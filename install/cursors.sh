f520ac7dbbb20a6d24d4735743dd518aa455bb0c
flamedfury <heavy.job8629@fastmail.com>
2024-06-11 21:51:17 +1200

    gnome-terminal

--- install/cursors.sh
+++ install/cursors.sh
@@ -4,4 +4,3 @@ cp ~/.local/share/omakub/cursors/Nordic-cursors.tar.xz ~/.icons/
 cd ~/.icons
 tar -xf Nordic-cursors.tar.xz
 rm -f Nordic-cursors.tar.xz
-gsettings set org.gnome.desktop.interface cursor-theme "Nordic-cursors"
