f2a97396764fe5467a9d0916228bd8ae53fbdc4a
flamedfury <heavy.job8629@fastmail.com>
2024-06-14 19:43:00 +1200

    updates

--- install/set-gnome-extensions.sh
+++ install/set-gnome-extensions.sh
@@ -1,25 +1,23 @@
-sudo apt install -y gnome-shell-extension-manager pipx
-pipx install gnome-extensions-cli --system-site-packages
+# sudo apt install -y gnome-shell-extension-manager pipx
+# pipx install gnome-extensions-cli --system-site-packages
 
-# Turn off default Ubuntu extensions
-# gnome-extensions disable tiling-assistant@ubuntu.com
-# gnome-extensions disable ubuntu-appindicators@ubuntu.com
-# gnome-extensions disable ubuntu-dock@ubuntu.com
-# gnome-extensions disable ding@rastersoft.com
+# # Turn off default Ubuntu extensions
+# # gnome-extensions disable tiling-assistant@ubuntu.com
+# # gnome-extensions disable ubuntu-appindicators@ubuntu.com
+# # gnome-extensions disable ubuntu-dock@ubuntu.com
+# # gnome-extensions disable ding@rastersoft.com
 
-# Pause to assure user is ready to accept confirmations
-gum confirm "To install Gnome extensions, you need to accept four confirmations. Are you ready?"
+# # Pause to assure user is ready to accept confirmations
+# gum confirm "To install Gnome extensions, you need to accept four confirmations. Are you ready?"
 
-# Install new extensions
-gext install space-bar@luchrioh
-gext install audio-output-switcher@anduchs
+# gext install audio-output-switcher@anduchs
 
-# Compile gsettings schemas in order to be able to set them
-sudo cp ~/.local/share/gnome-shell/extensions/space-bar\@luchrioh/schemas/org.gnome.shell.extensions.space-bar.gschema.xml /usr/share/glib-2.0/schemas/
-sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
+# # Compile gsettings schemas in order to be able to set them
+# sudo cp ~/.local/share/gnome-shell/extensions/space-bar\@luchrioh/schemas/org.gnome.shell.extensions.space-bar.gschema.xml /usr/share/glib-2.0/schemas/
+# sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
 
-# Configure Space Bar
-gsettings set org.gnome.shell.extensions.space-bar.behavior smart-workspace-names false
-gsettings set org.gnome.shell.extensions.space-bar.shortcuts enable-activate-workspace-shortcuts false
-gsettings set org.gnome.shell.extensions.space-bar.shortcuts enable-move-to-workspace-shortcuts true
-gsettings set org.gnome.shell.extensions.space-bar.shortcuts open-menu "@as []"
+# # Configure Space Bar
+# gsettings set org.gnome.shell.extensions.space-bar.behavior smart-workspace-names false
+# gsettings set org.gnome.shell.extensions.space-bar.shortcuts enable-activate-workspace-shortcuts false
+# gsettings set org.gnome.shell.extensions.space-bar.shortcuts enable-move-to-workspace-shortcuts true
+# gsettings set org.gnome.shell.extensions.space-bar.shortcuts open-menu "@as []"
